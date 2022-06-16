fc6f54c32ecfcc35ea92f49b39f252682b5021a2
urlastway <andriy.semenchenko13@gmail.com>
2022-06-16 17:14:47 +0200

    add prenotation

--- src/components/blocks/HeaderComponents/HeaderButtom.vue
+++ src/components/blocks/HeaderComponents/HeaderButtom.vue
@@ -9,7 +9,12 @@
           </div>
           <div clss="header__buttom__right">
               <div class="prenotation">
-
+                <h1 class="title__right">First Leson Free!</h1>
+                <p class="subtitle__right">When you make a block booking with us your first lesson is included free</p>
+                <form action="" v-for="(data, index) in datas" :key="index">
+                  <input type="text" :placeholder="data.name" v-modul="data.user__information"/>
+                </form>
+                <button class="header__bottom__button"><p>Request a callback</p></button>
               </div>
           </div>
       </div>
@@ -20,6 +25,28 @@
 <script>
 export default {
     name: 'HeaderButtom',
+    data(){
+      return {
+        datas: [
+          {
+            name: "Your Name*",
+            user__information: ""
+          },
+          {
+            name: "Email",
+            user__information: ""
+          },
+          {
+            name: "Telephone",
+            user__information: ""
+          },
+          {
+            name: "Location",
+            user__information: ""
+          },
+        ]
+      }
+    }
 }
 </script>
 
@@ -53,6 +80,11 @@ export default {
   background-color: #fff;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
   }
 .title{
   font-size: 2.5rem;
@@ -68,4 +100,57 @@ export default {
   font-weight: 300;
   line-height: 2;
   }
+.title__right{
+  color: var(--color__cube__BayLeaf);
+  font-weight: 500;
+  margin-bottom: 20px;
+}
+.subtitle__right{
+  text-align: center;
+  line-height: 2;
+  margin-bottom: 20px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  
+  input {
+    height: 45px;
+    border: solid 1px var(--color__cut__gumbo);
+    border-radius: 5px;
+    margin: 10px 0;
+    background-color: rgb(233, 233, 233);
+    font-size: 18px;
+    font-weight: 300;
+    padding: 10px;
+  }
+  input:hover{
+    border: solid 1px var(--color__cut__CapeCod);
+  }
+}
+
+/*--- Button ---*/
+.header__bottom__button {
+        margin-top: 20px;
+        cursor: pointer;
+        text-align: center;
+        text-decoration: none;
+        color: #fff;
+        padding: 15px 20px;
+        background: var(--color__cube__BayLeaf);
+        border: none;
+        border-radius: 20px;
+
+        p{
+            text-transform: uppercase;
+            font-weight: 700;
+            font-size: 15px;
+        }
+
+    }
+.header__bottom__button:hover{
+        background: var(--color__cut__BayLeaf);
+        transition: 0.3s;
+    }
 </style>
